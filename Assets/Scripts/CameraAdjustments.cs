bf7031b86fe4c0e1a80ba8a213582aa9a8f8e310
Raúl Benítez Millet <benitezmillet.raul@gmail.com>
2025-04-10 14:41:15 +0200

    Pruebas para la creacion procedural

---
+++ Assets/Scripts/CameraAdjustments.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjustments : MonoBehaviour
+{
+    private Camera m_camera;
+
+    void Start()
+    {
+        m_camera = GetComponent<Camera>();
+
+        AdjustCameraOrtographicSize();
+    }
+
+    void AdjustCameraOrtographicSize()
+    {
+        float baseSize = 5f;
+        float targetAspect = 16 / 9f;
+        float currentAspect = (float)Screen.width / Screen.height;
+
+        m_camera.orthographicSize = baseSize * (targetAspect / currentAspect);
+    }
+}
+ 
